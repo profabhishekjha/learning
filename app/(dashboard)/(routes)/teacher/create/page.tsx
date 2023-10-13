761c49af2eff237d02eb34abab0a07ac80a592fd
profabhishekjha <jhaabhishek910@gmail.com>
2023-10-13 17:51:27 +0530

    imports

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -2,6 +2,12 @@
 
 import * as z from "zod";
 import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+
+const formSchema = z.object({});
+
 const CreatePage = () => {
   return <div>Create Page</div>;
 };
